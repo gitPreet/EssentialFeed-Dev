2d70f82677b05f6a66b633ec13e7b3a0d1c65853
Preeetham Baliga <pbaliga@microsoft.com>
2022-06-01 18:26:28 +0530

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Preetham Baliga on 01/06/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
