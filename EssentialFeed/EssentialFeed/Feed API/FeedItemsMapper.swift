7d36a9f76a5ef1119c1710fba34b2ab816d571fd
Preeetham Baliga <pbaliga@microsoft.com>
2022-06-17 19:15:32 +0530

    Move all mapping logic to the FeedItemsMapper

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -13,6 +13,10 @@ internal struct FeedItemsMapper {
 
     private struct Root: Decodable {
         let items: [Item]
+
+        var feed: [FeedItem] {
+            return items.map { $0.item }
+        }
     }
 
     private struct Item: Decodable {
@@ -36,5 +40,14 @@ internal struct FeedItemsMapper {
         }
         let root = try JSONDecoder().decode(Root.self, from: data)
         return root.items.map { $0.item }
+    }
+
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+
+        guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(.invalidData)
+        }
+
+        return .success(root.feed)
     }
 }
