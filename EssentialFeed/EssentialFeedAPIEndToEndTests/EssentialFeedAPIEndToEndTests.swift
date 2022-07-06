7aa1bfee3ad95257917bb5022a008cbaed5783fb
Preeetham Baliga <pbaliga@microsoft.com>
2022-07-06 19:43:31 +0530

    Update EssentialFeedAPIEndToEndTests.swift

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -45,7 +45,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
          receivedResult = result
          exp.fulfill()
         }
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 15.0)
 
         return receivedResult
     }
